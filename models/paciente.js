de549a9cfc4c19b4c76876220a0883cdfe9a0cae
Javier Tello <jtello90@gmail.com>
2018-04-28 09:38:45 -0600

    Historial Medico por Paciente

--- models/paciente.js
+++ models/paciente.js
@@ -128,26 +128,75 @@ mdlPaciente.DespachoReceta = function(data, callback){
 	}
 }
 
+
+mdlPaciente.HistorialPaciente = function(data, callback){
+	try {
+		if (dbConn){
+			//var sql = 'call sp_traslado_paciente(?,?,?);';
+			var sql = 'call sp_obtiene_historial_medico (?);';
+			var params = [data.dpi];
+			var resultado = {
+				Exito:0,
+				DPI: data.dpi,
+				Nombre: '',
+				Apellido: '',
+				Telefono: '',
+				Direccion: '',
+				Fecha_Nacimiento:'',
+				Historial: [],
+				Traslados: []
+			};
+			dbConn.query(sql,params,function(error, result, fields){
+				if (error){
+					callback (null, resultado);
+				}
+				else {
+					if (result[0].length > 0) {
+						resultado.Exito = 1;
+						resultado.Nombre = result[0][0].Nombre;
+						resultado.Apellido = result[0][0].Apellido;
+						resultado.Telefono = result[0][0].Telefono;
+						resultado.Direccion = result[0][0].Direccion;
+						resultado.Fecha_Nacimiento = result[0][0].Fecha_Nacimiento;
+						if (result[1].length > 0) {
+							var cant = result[1].length;
+							for(var i=0; i<cant; i++){
+								var Historia = {Fecha:'',Descripcion:'',Diagnosticos:[], Receta:[]};
+								Historia.Descripcion = result[1][i].Descripcion;
+								Historia.Fecha = result[1][i].Fecha;
+								var Diagnostico = {Diagnostico: result[1][i].Diagnostico};
+								Historia.Diagnosticos.push(Diagnostico);
+								var receta = {Medicamento: result[1][i].Medicamento};
+								Historia.Receta.push(receta);
+								resultado.Historial.push(Historia);
+							}
+						}
+						if (result[2].length > 0) {
+							var cant2 = result[2].length;
+							for(var j=0; j<cant2; j++){
+								var traslado = {Origen:'',Destino:'',Fecha:''};
+								traslado.Origen = result[2][j].Origen;
+								traslado.Destino = result[2][j].Destino;
+								traslado.Fecha = result[2][j].Fecha;
+								resultado.Traslados.push(traslado);
+							}
+						}
+						callback (null, resultado);
+					}
+					else {
+						callback (null, resultado);
+					}
+				}
+			});
+		}
+	}
+	catch(err){
+		var msj = "Error al trasladar paciente, error:" + err.message;
+		console.log("Error al trasladar paciente, error:" + err.message);
+		var resultado = {"Exito":0, "Error":msj}
+		callback(null, resultado);
+	}
+}
+
 //exportamos el objeto para tenerlo disponible en la zona de rutas
 module.exports = mdlPaciente;
-/*
-{
-“Despachos”:[
-{
-“Receta”:”24”,
-“Fecha”:”19-04-2017”,
-“DPI”:”2114955650101”,
-“Medicamentos”:[
-{
-“Codigo”:”7788”,
-“Nombre”:”Paracetamol”,
-“Cantidad”:”11”
-},
-{
-“Codigo”:”2799”,
-“Nombre”:”Aspirina”,
-“Cantidad”:”21”
-}]
-}
-}
-*/
--- routes/paciente.js
+++ routes/paciente.js
@@ -59,6 +59,25 @@ module.exports = function(app) {
     }
   });
 
+  app.post("/HistorialPaciente", function(request,res){
+    try {
+      var parametros = {dpi:request.body.DPI};
+      mdlPaciente.HistorialPaciente(parametros, function(error, data){
+        if (data && !(data.error)){
+          res.status(200).json(data);
+        }
+        else {
+            //res.status(500).json(data.resultado);
+            res.status(200).json(data);
+        }
+      });
+    }
+    catch(err){
+      console.log("Error:" + err.message);
+      res.status(500).json(err.message);
+    }
+  });
+
 
   app.post("/obtenerPaciente", function(req,res){
     try {
